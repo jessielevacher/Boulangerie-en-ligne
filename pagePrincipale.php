ef3b9f81b4d7963a6b3093d81d247f9371471834
maaareme <mareme.thiam@insa-rouen.fr>
2017-05-02 14:52:43 +0200

    CSS et page principale

--- pagePrincipale.php
+++ pagePrincipale.php
@@ -5,24 +5,30 @@
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<link href="CSS/style.css" rel="stylesheet" type="text/css" />
 		<?php
-			require("Panier.class.php");
+		/*	require("Panier.class.php"); */
 		?>
+			<h1> Boulangerie "Les cinq gourmandes" </h1>
 	</head>
 
 	<body>
-		<h1> Boulangerie "Les 5 gourmandes" </h1>
+
 		</header>
 
+<nav>
+	<div id="barre_menu">
+	<div class="lien">	<a href="commandes.php"> Mes commandes en cours </a> </div>
+	<div class="lien">	<a href="panier.php"> Mon panier </a> </div>
+	<div class="lien">	<a href="tmpDeconnection.php"> Se déconnecter </a> </div>
+	</div>
+</nav>
 
-<a href="commandes.php"> Mes commandes en cours </a>
-<a href="panier.php"> Mon panier </a>
-<a href="tmpDeconnection.php"> Se déconnecter </a>
-
+<section>
 <figure>
+		<div id="images_produit">
 <a href="pageGateau.php">
   <img src="images/gateau.jpeg" alt="Gâteaux"/ width="100" height="100" >
 </a>
-
+ /* Mettre balises div pour les images renvoyant aux pages de produit*/
 <a href="pageViennoiserie.php">
   <img src="images/viennoiserie.jpeg" alt="Pains/Viennoiseries"/ width="100" height="100">
 </a>
@@ -30,9 +36,13 @@
 <a href="pageChocolat.php">
   <img src="images/chocolat.jpeg" alt="Chocolats"/ width="100" height="100">
 </a>
-</figure>
+	</div>
 
+</figure>
+</section>
+<footer>
+Ce site est dévelopé dans le cadre d'un projet en GM4.
+</footer>
 
 </body>
-
 </html>
