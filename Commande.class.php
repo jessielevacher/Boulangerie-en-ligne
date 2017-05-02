da48c4c8ac002104a8520a0348fef05da32d1a69
jlevacher <jessie.levacher@insa-rouen.fr>
2017-05-02 09:43:17 +0200

    Modification classe Commande

--- Commande.class.php
+++ Commande.class.php
@@ -10,7 +10,11 @@ date 1/05/2017 -->
     protected $moment;
     protected $prixTotal;
 
-    public function __construct($panier, $num, $jour, $mois, $annee, $moment) {
+    public function __construct($panier, $jour, $mois, $annee, $moment) {
+      $alphabet="0123456789abcdefghijklmnopqrstuvwxyz";
+      $num = '';
+      for($i=0; $i < 10; $i++)
+	       $num=$num.$alphabet[rand(0,35)];
       $this->numeroCommande = $num;
       $this->date = [$jour,$mois,$annee];
       $this->moment = $moment;
