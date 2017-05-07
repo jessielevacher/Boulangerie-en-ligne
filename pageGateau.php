a1a0eb0d829954f025051025f60cb7d7362a2f05
gakki88 <tangtang5yo@gmail.com>
2017-05-07 21:36:04 +0800

    Update pageGateau.php

--- pageGateau.php
+++ pageGateau.php
@@ -1,19 +1,48 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Page des gateaux !</title>
+		<title>Page des gateaux </title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<link href="CSS/style.css" rel="stylesheet" type="text/css" />
 		<link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet" type="text/css">
 		<?php
-
+			require("Boulangerie.class.php");
+			require("Panier.class.php");
+			session_start();
+			$panier = $_SESSION['panier'];
 		?>
 	</head>
 
-	<body>
+<body class="article">
+		<header id="top">
+			<a href="pagePrincipale.php" id="logo"> <input type="button" value="Déconnexion"> </a>
 		<h1> Boulangerie "Les 5 gourmandes" </h1>
-    Bienvenue sur la page des gateaux.
+    Bienvenue sur la page des pains et viennoiseries.
 		</header>
-  </body>
+	
+<form action="traiterArticles.php" method="POST">
+	<div class="centrage">
+	
+	<table class="article">
+			<tr class="article">
+				<th class="article">Nos Gâteaux</th>
+				<th class="article">Prix</th>
+				<th class="article">Quantité</th>
+			</tr>
+		<?php
+				AffichageGateau();
+		?>
+		
+	</table>
+	<br/>
+	<a href="pagePrincipale.php"> <input type="button" value="Retour"> </a>
+	
+	<input type="submit" name="action" value="Valider"/>
+	<input type="submit" name="effacer" value="Effacer"/>
+	</div>
+</form>
+
+</body>
+
 
 </html>
