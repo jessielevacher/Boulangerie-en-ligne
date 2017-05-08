1cc3f920ab2db70ceb460857e0dbf25a7853a995
gakki88 <tangtang5yo@gmail.com>
2017-05-08 22:40:02 +0800

    Update pageGateau.php

--- pageGateau.php
+++ pageGateau.php
@@ -14,7 +14,7 @@
 		?>
 	</head>
 
-<body class="article">
+<body class="gateau">
 		<header id="top">
 			<a href="pagePrincipale.php" id="logo"> <input type="button" value="Déconnexion"> </a>
 		<h1> Boulangerie "Les 5 gourmandes" </h1>
