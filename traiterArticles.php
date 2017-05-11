9a9655ef9d6c4084e859a9debc2de144b75ebe19
maaareme <mareme.thiam@insa-rouen.fr>
2017-05-11 23:58:09 +0200

    Commentaires

--- traiterArticles.php
+++ traiterArticles.php
@@ -4,6 +4,8 @@
  session_start();
  $client = $_SESSION['client'];
 
+/* Si l'utilisateur a validé des articles de la page des gâteaux, on ajoute au panier les articles qui ont été sélectionnés 
+dans les quantités correspondantes. On redirige ensuite l'utilisateur vers la page principale */
 	  if (isset( $_POST ["actionG"] )){
 		  for($i = 0; $i < 18 ; $i++){
         if(!empty ($_POST [strval($i)])) {
@@ -14,6 +16,8 @@
       header("Location:pagePrincipale.html");
     }
 
+/* Si l'utilisateur a validé des articles de la page des pains et viennoiseries, on ajoute au panier les articles
+qui ont été sélectionnés dans les quantités correspondantes. On redirige ensuite l'utilisateur vers la page principale */
     if (isset( $_POST ["actionP"] )){
   		  for($i = 18; $i < 36; $i++){
           if(!empty ($_POST [strval($i)])) {
@@ -23,6 +27,9 @@
   		  }
         header("Location:pagePrincipale.html");
 	  }
+
+/* Si l'utilisateur a validé des articles de la page des chocolats, on ajoute au panier les articles qui ont été sélectionnés 
+dans les quantités correspondantes. On redirige ensuite l'utilisateur vers la page principale */
 
     if (isset( $_POST ["actionC"] )){
 		  for($i = 36; $i < 54; $i++){
